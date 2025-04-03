464d1af09d798c189ee5aa99b7efdd15aad39399
Sarah Behrendt <sarahelna1994@gmail.com>
2025-04-03 12:05:47 +0200

    backup

---
+++ docker/html/index.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h1>PHP funktioniert!</h1>";
+phpinfo();
+?>
---
+++ docker/public/test_db.php
@@ -0,0 +1,13 @@
+<?php
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASSWORD');
+$db   = getenv('DB_NAME');
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Fehler: " . $conn->connect_error);
+} 
+echo "Datenbankverbindung erfolgreich!";
+?>
